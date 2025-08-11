43691cb563ad885cd5e7898a4f42e1145ba206a5
mayro <mayro@DOCZIN>
2025-08-11 18:22:01 -0300

    Adicionar arquivos de projeto.

---
+++ GertecServer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GertecServer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
